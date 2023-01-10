7f1e4ac694fc933af3f265ef320081511413c07f
maxime masson <maxime.masson@monespaceproprio.com>
2023-01-10 11:27:17 -0500

    add move bindsym to i3 config

---
+++ .config/i3/pwa-teams.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+google-chrome-stable --profile-directory=Default --app-id=cifhbcnohmdccbgoicgdjpfamggdegmo
+#microsoft-edge --profile-directory=Default --app-id=kchpfoeacdfklikaefkklcbokboadclf "--app-url=https://teams.microsoft.com/_#/calling/19:meeting_NmJkZTUwODQtMzdiNC00MzQyLWE3NjktZjBiYTExN2U0MTZl@thread.v2/"
+
