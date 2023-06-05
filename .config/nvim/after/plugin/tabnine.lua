8c5c1d34a36d7daf18fa741e9be2438baa4cd222
Maxime Masson <maxime.masson@nurun.com>
2023-06-05 16:30:10 -0400

    fix: tabnine completion shortcut collide with cmp

--- .config/nvim/after/plugin/tabnine.lua
+++ .config/nvim/after/plugin/tabnine.lua
@@ -1,7 +1,7 @@
 require("tabnine").setup({
   disable_auto_comment = true,
   accept_keymap = "<C-q>",
-  dismiss_keymap = "<C-w>",
+  dismiss_keymap = "<C-]>",
   debounce_ms = 800,
   suggestion_color = { gui = "#808080", cterm = 244 },
   exclude_filetypes = { "TelescopePrompt" },
