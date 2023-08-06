ef3863357f83df1298ca7b5713262ea3d3d8ede0
Maxime Masson <maximemasson30@gmail.com>
2023-08-06 09:17:11 -0400

    feat: add trouble and harpoon plugin

--- .config/nvim/lua/ronmasson/packer.lua
+++ .config/nvim/lua/ronmasson/packer.lua
@@ -69,7 +69,18 @@ packer.startup(function(use)
       { "theHamsta/nvim-dap-virtual-text" },
     },
   })
-
+  use({
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup({
+        icons = false,
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  })
+  use("folke/zen-mode.nvim")
   use("mfussenegger/nvim-lint")
 
   use("numToStr/FTerm.nvim")
