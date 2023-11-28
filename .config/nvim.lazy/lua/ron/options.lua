66feb2b43caa2aa9632f7d7ae67fc0a11c448d97
Maxime Masson <maximemasson30@gmail.com>
2023-11-28 17:16:58 -0500

    fix: backward compatibility ts-comment-string

--- .config/nvim.lazy/lua/ron/options.lua
+++ .config/nvim.lazy/lua/ron/options.lua
@@ -1,6 +1,7 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
+vim.g.skip_ts_context_commentstring_module = true
 
 -- This makes all clipboard under the same. OS clipboard is accessible with P and vice-versa
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
