98ec7510f9088a2a7953ecc5abd040cefd3e9dc4
Maxime Masson <maxime.masson@nurun.com>
2023-12-12 11:07:44 -0500

    feat: add keumap to open and close diffview plugin

--- .config/nvim.lazy/lua/plugins/diffview.lua
+++ .config/nvim.lazy/lua/plugins/diffview.lua
@@ -1,3 +1,15 @@
 return {
   "sindrets/diffview.nvim",
+  keys = {
+    {
+      "<leader>do",
+      ":DiffviewOpen<CR>",
+      desc = "Open Diffview tool",
+    },
+    {
+      "<leader>dc",
+      ":DiffviewClose<CR>",
+      desc = "Close Diffview tool",
+    },
+  },
 }
